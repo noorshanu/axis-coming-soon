d45106106b38a3bf0f2bc9974b44ed14f1faa522
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-03-30 23:37:06 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
-
+import Top from './assets/top.png'
+import Logo from './assets/logo.png'
 function App() {
   return (
-    <div className="App">
+    <div className="main ">
+      <img src={Top} alt='' className='top-img'/>
+      <div className='logo-div'>
+        <img src={Logo} alt=''/>
+
+      </div>
+      <div>
       <h1>new</h1>
+      </div>
+    
     </div>
   );
 }
