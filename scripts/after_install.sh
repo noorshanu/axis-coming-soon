9c3a60a8411a5d6be1ab0a111d399b161a6d9371
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-09-13 22:58:34 +0530

    new

---
+++ scripts/after_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /var/www/html/build
+
+#Install_node_modules
+npm  install
+npm run build
---
+++ scripts/before_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /var/www/html/build
+
+#_Update_&_Set_Node_Version
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#_Download_Node_&NPM
+sudo apt-get install -y nodejs
+
+sudo npm install pm2 -g
+
+# You may also need development tools to build native addons
+sudo apt-get install gcc g++ make
+
+# To install the Yarn package manager
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
